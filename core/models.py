a88f473c7e7b67d502eb6cb9e4c868868fa80f68
Wei-Han He <scsa32@gmail.com>
2023-10-08 19:44:21 +0800

    Category, Tag, Source. Simplify Video.

--- core/models.py
+++ core/models.py
@@ -1,10 +1,32 @@
+from django.conf import settings
 from django.db import models
 
 
-class Video(models.Model):
-    path = models.FilePathField()
-    porn_id = models.CharField(max_length=100)
-    # height = models.IntegerField()
-    # width = models.IntegerField()
-    # format = models.CharField(max_length=10)
+class Category(models.Model):
+    title = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Source(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class Video(models.Model):
+    file = models.FileField(upload_to='upload', unique=True)
+    source = models.ForeignKey(Source, on_delete=models.CASCADE, null=True, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+    review = models.IntegerField(null=True, blank=True)
+    like = models.BooleanField(null=True, blank=True)
